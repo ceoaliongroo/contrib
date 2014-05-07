52af22927dbbc544356fc872b5fb7e4ede887ef9
Carlos Mantilla <cmantilla@aliongroo.com>
2014-05-07 10:12:11 +0300

    Initial commit application AngularJS
    generated by yeoman.io.

---
+++ angular-travis/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTravisApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
